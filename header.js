04ca7f81ae819585e498910bf93976a2e7327b83
Alexander <allk@seges.dk>
2024-04-30 20:11:21 +0200

    Initial Sweep

--- header.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('headerContainer').innerHTML = `
-<p id="subtext"><br /> <br /> <br />  
-        Color palette from <a  id="subtext" href="https://github.com/catppuccin/catppuccin">Catppuccin</a> (Macchiato).<br />
-        2024 Max Jónsbur. <a id="subtext" href="https://github.com/vqia/vqia.github.io">Source Code</a></p>
-`
